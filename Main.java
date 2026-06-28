0c24d2c78b6e1f9cb5866cbfc5b15ae6253794c0
LVL2 LEARNER <amiirzaydo@gmail.com>
2026-06-28 23:42:55 +0000

    Rename diviser method to division in Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("Addition: " + calc.addition(10, 5));
         System.out.println("Soustraction: " + calc.soustraction(10, 5));
         System.out.println("Multiplication: " + calc.multiplier(10, 5));
-        System.out.println("Division: " + calc.diviser(5, 0));
+        System.out.println("Division: " + calc.division(5, 0));
         System.out.println("Racine carrée: " + calc.racineCarree(25));
     }
 }

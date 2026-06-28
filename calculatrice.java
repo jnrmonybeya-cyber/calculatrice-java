c3227cf98fb03858573e956265b00abff9d7851b
Yacine Ouedraogo <yacineouedraogo36@gmail.com>
2026-06-28 22:12:50 +0000

    Ajout d'une nouvelle branche

--- calculatrice.java
+++ calculatrice.java
@@ -1,3 +1,8 @@
 public class calculatrice {
-    
+    public double diviser(double a, double b){
+        return a / b;
+    }
+    public double racineCarree(double a){
+        return Math.sqrt(a);
+    }
 }
